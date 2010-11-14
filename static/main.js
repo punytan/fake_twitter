633e9656597f43d2ab5a54e8d5ddd7afae03191b
punytan <punytan@gmail.com>
2010-11-14 23:46:00 +0900

    emphasize tweet that refers to me

--- static/main.js
+++ static/main.js
@@ -19,6 +19,7 @@ function load(v) {
                 $("div#timeline").append( $('<div>').attr({id:id}) );
 
                 if (r[i].retweeted_status.text) $("div#" + id).css("background-color", "#ffc");
+                if (/pun[y|i]tan/.test(r[i]["processed"])) $("div#" + id).css("background-color", "pink");
 
                 $("div#" + id).append(
                     $('<div>').append(
